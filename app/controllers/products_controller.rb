efa836c4e407dfb88bcd208e46e72cdb3041d5d5
carterdboyle <carterboyle@msn.com>
2025-09-03 12:36:36 -0300

    Added in search functionality - with SQL validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def search
+    @products = Product.where("title LIKE ?", "%#{Product.sanitize_sql_like(params[:query])}%")
+    render 'products/index'
+  end
+
 end
