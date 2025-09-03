1ed50a67d784da748021c2f0376e4eef5340f4de
Carter Boyle <carterboyle@msn.com>
2025-09-02 23:41:50 -0300

    More layout - on index page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+  TITLE_LENGTH = 25
+
+  def title_trimmer(title)
+    if title && title.length > TITLE_LENGTH
+      title[0..TITLE_LENGTH] + "..."
+    else
+      title
+    end
+  end
 end
