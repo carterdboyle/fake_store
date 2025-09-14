ea88ec4705160107451d8ba985c4c71307c367ba
carterdboyle <carterboyle@msn.com>
2025-09-13 22:29:47 -0300

    Adding back in landing page and fixing favicon

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "products#index"
+  root "pages#index"
 
   resources :products, only: [:index, :show, :update]
   resource :cart, only: [:show, :destroy]
